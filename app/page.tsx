90976688370fe2b2cf8182ffdc0ae72668140380
Rodrigo Santos <hello@rodrigosantos.dev>
2026-04-24 17:20:49 +0100

    Add structured JSON-LD metadata to the homepage
    Introduced a JSON-LD script to the homepage for enhanced SEO, providing structured data about the person represented on the site, including job title, social profiles, and organizational affiliation. This addition aims to improve search engine visibility and user engagement.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "Person",
+  image: "https://rodrigosdev.com/opengraph-image.png",
+  jobTitle: "AI Engineer",
+  name: "Rodrigo Santos",
+  sameAs: [
+    "https://x.com/rrcssantos",
+    "https://www.linkedin.com/in/rrcssantos/",
+    "https://github.com/rodrigosdev",
+  ],
+  url: "https://rodrigosantos.dev",
+  worksFor: {
+    "@type": "Organization",
+    name: "Snyk",
+  },
+};
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="font-pixel font-bold text-4xl">Coming Soon ...</h1>
-      <p className="text-lg font-mono">I'm currently working on it</p>
-    </div>
+    <>
+      <script type="application/ld+json">{JSON.stringify(jsonLd)}</script>
+    </>
   );
 }
