072cbf4d1db090c000270d501168c5474918ac04
Rodrigo Santos <hello@rodrigosantos.dev>
2026-04-27 19:30:55 +0100

    Remove globals.css and update layout.tsx to import styles from a new path; modify page.tsx to adjust the height of the homepage component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "~/lib/styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +31,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Hello World</div>;
+  return <div className="h-[300vh]">Hello World</div>;
 }
