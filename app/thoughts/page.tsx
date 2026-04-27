58087794ed462263ec1c3f34f45d253388c921a9
Rodrigo Santos <hello@rodrigosantos.dev>
2026-04-27 16:59:22 +0100

    Enhance Thoughts page layout and metadata
    Updated the Thoughts page to include a metadata title and improved the layout with a new structure and styling for better visual presentation. Added a blinking animation effect to the title for enhanced engagement.

--- app/thoughts/page.tsx
+++ app/thoughts/page.tsx
@@ -1,5 +1,18 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Thoughts",
+};
+
 const Thoughts = () => {
-  return <div>Thoughts</div>;
+  return (
+    <div className="px-4 sm:px-0 max-w-full sm:max-w-lg w-full mx-auto text-xs pt-0 xl:pt-14 pb-14">
+      <h1 className="text-left text-white mb-1">
+        Thoughts<span className="animate-blink">_</span>
+      </h1>
+      <p className="text-left mb-4">Writing and Reflections</p>
+    </div>
+  );
 };
 
 export default Thoughts;
